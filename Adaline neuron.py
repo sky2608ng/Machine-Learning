23f1468e72bff492baff2c10c7d4aa91acc4ce13
sky2608ng <62634215+sky2608ng@users.noreply.github.com>
2021-03-15 16:17:03 +0800

    Chapter3 notes

--- Adaline neuron.py
+++ Adaline neuron.py
@@ -119,3 +119,117 @@ plt.plot(range(1, len(ada.errors_) + 1), ada.errors_, marker = 'o')
 plt.xlabel('Epochs')
 plt.ylabel('Sum-squared-error')
 plt.show()
+
+from numpy.random import seed
+
+class AdalineSGD(object):
+    """Adaptive linear neuron classifier
+
+    Parameter
+    ----------
+    eta: floating
+        Learning rate between 0.0 and 1.0
+    n_iter: int
+        Passes over the training dataset
+
+    Attributes
+    ----------
+    w_: 1-d array
+        Weights after fitting
+    errors_: list
+        Number of misclassifications in every epoch
+    shuffle: bool (default: True)
+        Shuffles training data every epoch
+        if True to prevent cycles
+    random_state: int (default: None)
+        Set random satet for shuffling and initializing the weights
+    """
+    def __init__(self, eta = 0.01, n_iter = 10, shuffle = True, random_state = None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, x, y):
+        """Fit training dat
+
+        Parameters
+        ----------
+        x: {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and n_features is the number of features
+        y: array-like, shape = [n_samples]
+            Target values
+
+        Returns
+        -------
+        self: object
+        """
+
+        self._initialize_weights(x.shape[1])
+        self.errors_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                x, y = self._shuffle(x, y)
+            error = []
+            for xi, target in zip(x,y):
+                error.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.errors_.append(avg_cost)
+        return self
+    
+    def partial_self(self, x, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(x.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(x, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(x, y)
+        return self
+
+    def _shuffle(self, x, y):
+        """Shuffle training data"""
+        r = np.random.permutation(len(y))
+        return x[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to zero"""
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
+    
+    def net_input(self, x):
+        """Calculate net input"""
+        return np.dot(x, self.w_[1:]) + self.w_[0]
+
+    def activation(self, x):
+        """Compute linear activation"""
+        return self.net_input(x)
+
+    def predict(self, x):
+        """return class label after unit step"""
+        return np.where(self.activation(x) >= 0.0, 1, -1)
+
+ada = AdalineSGD(n_iter = 15, eta = 0.01, random_state = 1)
+ada.fit(x_std, y)
+plot_decision_regions(x_std, y, classifier = ada)
+plt.title("Adaline - Stochastic Gradient Descent")
+plt.xlabel('Sepal length [standardized')
+plt.ylabel('Petal length [standardized')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.plot(range(1, len(ada.errors_) + 1), ada.errors_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+plt.show()
---
+++ Chapter3 Scikit-learn.md
@@ -0,0 +1,7 @@
+# Chapter3
+
+1. Select Features
+2. Select Performance metric
+3. Select Classifier and Optimization Algorith
+4. Evaluate Model
+5. Tune algorithm
